39866972164aaa57c3a669bb645bd259ba8aaaa2
Andrew Cunningham <old.noakes@gmail.com>
2015-05-07 23:30:46 +1000

    typo in name of destination file

--- modules/test/manifests/template/undefined.pp
+++ modules/test/manifests/template/undefined.pp
@@ -2,7 +2,7 @@ class test::template::undefined($defined_value = 'this value is defined',
                                 $undefined_value = undef) {
 
   file { "undefined.test":
-     path => "$::local_tmp/undefined.text",
+     path    => "$::local_tmp/undefined.test",
      content => template('test/undefined.test.erb')
   }
 }
